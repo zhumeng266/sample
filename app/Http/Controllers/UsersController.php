c7d9b3033f3bb5b95a15689133bc54940596d4f4
vagrant <vagrant@homestead>
2018-02-13 09:03:01 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -82,29 +82,42 @@ class UsersController extends Controller
   }
 
   public function confirmEmail($token)
-   {
-       $user = User::where('activation_token', $token)->firstOrFail();
-
-       $user->activated = true;
-       $user->activation_token = null;
-       $user->save();
-
-       Auth::login($user);
-       session()->flash('success', '恭喜你，激活成功！');
-       return redirect()->route('users.show', [$user]);
-   }
-
-protected function sendEmailConfirmationTo($user)
   {
-      $view = 'emails.confirm';
-      $data = compact('user');
-      $from = 'aufree@yousails.com';
-      $name = 'Aufree';
-      $to = $user->email;
-      $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+    $user = User::where('activation_token', $token)->firstOrFail();
 
-      Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-          $message->from($from, $name)->to($to)->subject($subject);
-      });
+    $user->activated = true;
+    $user->activation_token = null;
+    $user->save();
+
+    Auth::login($user);
+    session()->flash('success', '恭喜你，激活成功！');
+    return redirect()->route('users.show', [$user]);
   }
+
+  protected function sendEmailConfirmationTo($user)
+  {
+    $view = 'emails.confirm';
+    $data = compact('user');
+    $from = 'aufree@yousails.com';
+    $name = 'Aufree';
+    $to = $user->email;
+    $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+
+    Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+      $message->from($from, $name)->to($to)->subject($subject);
+    });
+  }
+
+  public function followings(User $user){
+    $users=$user->followings()->paginate(30);
+    $title='关注的人';
+     return view('users.show_follow', compact('users', 'title'));
+  }
+
+  public function followers(User $user)
+   {
+       $users = $user->followers()->paginate(30);
+       $title = '粉丝';
+       return view('users.show_follow', compact('users', 'title'));
+   }
 }
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -8,11 +8,14 @@
           @include('shared._status_form')
         </section>
         <h3>微博列表</h3>
-       @include('shared._feed')
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+        <section class="stats">
+          @include('shared._stats', ['user' => Auth::user()])
         </section>
       </aside>
     </div>
